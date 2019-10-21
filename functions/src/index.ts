b3fcfc5fafc99c9e29d7697e1d50c81a2764d443
lukabuz <buzaladzeluka@gmail.com>
2019-10-20 22:07:33 -0400

    created express app on /api/ route, set up cors and parsing

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,31 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as express from "express";
+import * as cors from "cors";
+import * as parser from "body-parser";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// function for determining if a body is a json string
+const isJsonString = (str: any): boolean => {
+	try {
+		JSON.parse(str);
+	} catch (e) {
+		return false;
+	}
+	return true;
+};
+
+// middleware for parsing req.body
+const parsingMiddleware = (req: any, res: any, next: any) => {
+	if (isJsonString(req.body)) {
+		req.body = JSON.parse(req.body);
+	}
+	next();
+};
+
+// initialize express, set cors, and handle body parsing
+const api = express();
+api.use(cors({ origin: true }));
+api.use(parser.urlencoded({ extended: false }));
+api.use(parser.json());
+api.use(parsingMiddleware);
+
+exports.api = functions.https.onRequest(api);
